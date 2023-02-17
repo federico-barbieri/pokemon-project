7fe3fb5a2ff594f5c1d2e5f1d5c75161372e750d
Federico Barbieri <federicobarbieri54@gmail.com>
2023-02-17 22:39:30 +0100

    creating charmander pokecard

---
+++ src/Pokecard.js
@@ -0,0 +1,17 @@
+import React, {Component} from "react";
+
+const POKE_API = "https://raw.githubusercontent.com/POKEAPI/sprites/master/sprites/pokemon/";
+
+class Pokecard extends Component{
+    render(){
+        let imgSrc = `${POKE_API}${this.props.id}.png`;
+        return (
+            <div className="Pokecard">
+                <h1>{this.props.name}</h1>
+                <img src={imgSrc} />
+            </div>
+        )
+    }
+}
+
+export default Pokecard;
